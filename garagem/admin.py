22f1e311cf0ca7b9a018aaae51844da1133cdb21
felipejare <felipeakf@gmail.com>
2023-03-21 16:30:32 -0300

    Criação da tabela Acessorio, Cor e Veiculo

--- garagem/admin.py
+++ garagem/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from garagem.models import Marca, Categoria
+from garagem.models import Marca, Categoria, Veiculo, Cor, Acessorio
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Veiculo)
+admin.site.register(Cor)
+admin.site.register(Acessorio)
